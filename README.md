373defb2f737ebe03cc17f323c27e4248fa6bfd7
studiobloom <john.large.designs@gmail.com>
2023-05-18 15:07:10 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# opti-webp
-Opti-WebP bulk resizes, compresses and converts non WebP images to an optimized WebP final version
+# Opti-WebP
+
+Opti-WebP is an image optimization tool that enables bulk resizing, compression, and conversion of non-WebP images to an optimized WebP format. 
+It helps reduce image file sizes while maintaining visual quality, making it suitable for web development and optimization tasks.
+
+## Features
+
+- Bulk resize, compress, and convert non-WebP images to WebP format.
+- Limit the maximum width/height of images while preserving aspect ratio.
+- Supports various image formats, including PNG, JPEG, GIF, BMP, HEIC, TIFF, and TIF.
+- Optimized images are saved as WebP format, providing smaller file sizes.
+- Easy-to-use graphical user interface (GUI) for selecting the target directory and configuring the max dimension size.
+
+## Usage
+
+1. Download the latest release from the [Releases](https://github.com/your-username/opti-webp/releases) page.
+2. Extract the contents of the release package.
+3. Run the `opti-webp.exe` executable.
+4. Choose the directory containing the images you want to optimize.
+5. Set the maximum dimension size (width/height) for the images. (500px-4000px is suggested)
+6. Click the "OK" button to start the optimization process.
+7. Opti-WebP will resize, compress, and convert the images to WebP format.
+8. The optimized images will be saved in the same directory with "_resized.png" and ".webp" extensions.
+9. Once the optimization process is complete, you can find the optimized images in the target directory.
+
+## Credits
+
+Opti-WebP is created by John Large (aka bloom) and is available at [GitHub](https://github.com/your-username/opti-webp). 
+Visit the [website](https://studiobloom.xyz) for more information and updates.
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ opti-webp.py
@@ -0,0 +1,123 @@
+import ctypes
+import sys
+import os
+import tkinter as tk
+from tkinter import filedialog, simpledialog
+from PIL import Image
+
+def set_console_title():
+    ctypes.windll.kernel32.SetConsoleTitleW("Opti-WebP")
+
+def display_initial_message():
+    print("Opti-WebP - Image Optimization Tool")
+    print("Opti-WebP will bulk resize, compress and convert non WebP images to an optimized WebP final version.")
+    print("Created by John Large aka bloom")
+    print("Website: https://studiobloom.xyz")
+    print("If this tool helps you, please consider buying me donating:\n$studiobloomxyz on cash app, paypal.me/studiobloomxyz\nBTC @ 33bhGfzcKekYh8oB31Jzv5FYUkdahyC3eA\nETH @ 0xD974b9ab6e897d1128F2aFe98Aa172dE8180D27E")
+    print("\n\n")
+    print("Choose the directory of the image(s) to be optimized.\nProceed through the following window prompts.")
+
+def select_directory():
+    root = tk.Tk()
+    root.withdraw()
+    directory = filedialog.askdirectory()
+    return directory
+
+def get_icon_path():
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, 'opti-webp.ico')
+    else:
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'opti-webp.ico')
+
+class MaxDimensionSizeDialog(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Max Dimension Size")
+        self.iconbitmap(get_icon_path())
+        self.geometry("300x250")
+        self.resizable(False, False)
+
+        self.max_dimension_size = None
+        self.create_widgets()
+
+    def create_widgets(self):
+        label = tk.Label(self, text="Limit max width/height of image(s).\nAspect Ratio will remain locked.\n(between 500px-4000px is suggested)\nEnter the maximum dimension size:")
+        label.pack(pady=10)
+
+        self.entry = tk.Entry(self)
+        self.entry.pack()
+
+        button = tk.Button(self, text="OK", command=self.set_max_dimension_size)
+        button.pack(pady=10)
+        
+    def set_max_dimension_size(self):
+        try:
+            self.max_dimension_size = int(self.entry.get())
+        except ValueError:
+            pass
+        self.destroy()
+        
+def get_max_dimension_size():
+    root = tk.Tk()
+    root.withdraw()
+    dialog = MaxDimensionSizeDialog(root)
+    root.wait_window(dialog)
+    return dialog.max_dimension_size
+
+def count_images(directory):
+    image_count = sum([filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".heic", ".tiff", ".tif")) for filename in os.listdir(directory)])
+    print(f"Optimizable Images found in directory: {image_count}")
+    return image_count
+
+def resize_and_convert(directory, max_dimension_size):
+    image_count = count_images(directory)
+    if image_count == 0:
+        print("No optimizable images found.")
+        return
+
+    print(f"Processing images in directory: {directory}")
+    for filename in os.listdir(directory):
+        try:
+            if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".heic", ".tiff", ".tif")):
+                print(f"Processing image: {filename}")
+                img = Image.open(os.path.join(directory, filename))
+                img.thumbnail((max_dimension_size, max_dimension_size))
+
+                # Save as PNG
+                new_filename = os.path.splitext(filename)[0] + "_resized.png"
+                img.save(os.path.join(directory, new_filename), "PNG", optimize=True)
+                print(f"Saved resized image as: {new_filename}")
+
+                # Convert to WebP
+                webp_filename = os.path.splitext(filename)[0] + ".webp"
+                img.save(os.path.join(directory, webp_filename), "WEBP")
+                print(f"Converted image to WebP: {webp_filename}")
+
+                # Delete resized PNG file
+                os.remove(os.path.join(directory, new_filename))
+                print(f"Deleted resized image: {new_filename}")
+
+        except Exception as e:
+            print(f"An error occurred while processing image {filename}: {e}")
+
+if __name__ == "__main__":
+    set_console_title()
+    display_initial_message()
+    directory = select_directory()
+    if directory:
+        max_dimension_size = get_max_dimension_size()
+        if max_dimension_size:
+            resize_and_convert(directory, max_dimension_size)
+    
+    # Keep the command window open and prompt the user to restart
+    while True:
+        user_input = input("Your conversion is now complete, thank you for using Opti-WebP:)\nType 'r' to run the script again, or press enter to exit:")
+        if user_input.lower() == "r":
+            set_console_title()
+            directory = select_directory()
+            if directory:
+                max_dimension_size = get_max_dimension_size()
+                if max_dimension_size:
+                    resize_and_convert(directory, max_dimension_size)
+        else:
+            break
